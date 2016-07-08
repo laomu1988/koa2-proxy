8d1884734fda05f66e5ba21a4f4fe6e60676b453
zhilong mu <laomu1988@qq.com>
2016-07-08 23:00:42 +0800

    fullUrlbug： request.url有时会返回带有protocal和host的地址

--- src/method/fullUrl.js
+++ src/method/fullUrl.js
@@ -1,4 +1,7 @@
 module.exports = function () {
     var request = this.request;
+    if (request.url.indexOf('http') == 0) {
+        return request.url;
+    }
     return request.protocol + '://' + request.header.host + request.url;
 };
