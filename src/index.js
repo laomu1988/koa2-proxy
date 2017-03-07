12cb9f2322e5714951b16abcfc33a5b7bb667bed
muzhilong <laomu1988@qq.com>
2017-03-07 20:31:21 +0800

    multipart/form-data

--- src/index.js
+++ src/index.js
@@ -1,75 +1,80 @@
-require("babel-core/register");
-require("babel-polyfill");
+require('babel-core/register')
+require('babel-polyfill')
 // require('./assist/update');
-var koa = require('koa');
-var bodyParser = require('koa-bodyparser');
-var listen = require('./listen');
-var logger = require('logger-color');
-var match = require('koa2-match');
-var proxy = {};
-var app = new koa;
-proxy.app = app;
-proxy.use = app.use.bind(app);
-
+var koa = require('koa')
+var bodyParser = require('koa-body')
+var listen = require('./listen')
+var logger = require('logger-color')
+var match = require('koa2-match')
+var proxy = {}
+var app = new koa()
+proxy.app = app
+proxy.use = app.use.bind(app)
 
 proxy.static = function () {
-    app.use(require('./middleware/static').apply(proxy, arguments));
-};
+    app.use(require('./middleware/static').apply(proxy, arguments))
+}
 proxy.mockfile = function () {
-    app.use(require('./middleware/mockfile').apply(proxy, arguments));
-};
+    app.use(require('./middleware/mockfile').apply(proxy, arguments))
+}
 proxy.slow = function () {
-    app.use(require('./middleware/slow').apply(proxy, arguments));
-};
+    app.use(require('./middleware/slow').apply(proxy, arguments))
+}
 
 proxy.smarty = function () {
-    app.use(require('./middleware/smarty').apply(proxy, arguments));
-};
+    app.use(require('./middleware/smarty').apply(proxy, arguments))
+}
 
-
-var addMatched = false;
+var addMatched = false
 proxy.when = function () {
-    match.match.apply(match, arguments);
-    if(!addMatched) {
-        addMatched = true;
-        app.use(match.callback());
+    match.match.apply(match, arguments)
+    if (!addMatched) {
+        addMatched = true
+        app.use(match.callback())
     }
-};
+}
 
-//proxy.browser = function (server) {
+// proxy.browser = function (server) {
 //    require('./browser').bind(proxy)(server);
-//};
+// };
 
+proxy.listen = listen.bind(proxy)
+proxy.logger = logger
+proxy.open = require('./open').bind(proxy)
 
-proxy.listen = listen.bind(proxy);
-proxy.logger = logger;
-proxy.open = require('./open').bind(proxy);
-
-var Event = require('events').EventEmitter;
-var event = new Event();
+var Event = require('events').EventEmitter
+var event = new Event()
 proxy.on = proxy.bind = function () {
-    event.on.apply(event, arguments);
-};
+    event.on.apply(event, arguments)
+}
 
 proxy.emit = proxy.trigger = function () {
-    event.emit.apply(event, arguments);
-};
+    event.emit.apply(event, arguments)
+}
 
 // 初始化
-logger.setLevel('notice');
-require('./extends_proxy')(proxy);
-proxy.use(bodyParser());
-proxy.use(require('./extends_ctx').bind(proxy)());
-
+logger.setLevel('notice')
+require('./extends_proxy')(proxy)
+proxy.use(bodyParser({multipart: true}))
+proxy.use(function (ctx, next) {
+    var body = ctx.request.body
+    if (body) {
+        ctx.request._body = body
+        ctx.request.body = body.fields
+        if (body.files) ctx.request.files = body.files
+    }
+    return next()
+})
+proxy.use(require('./extends_ctx').bind(proxy)())
 
 // 错误处理
 process.on('uncaughtException', function (err) {
-    proxy.logger.error('uncaughtException', err, err.stack);
-});
+    proxy.logger.error('uncaughtException', err, err.stack)
+})
 
 process.on('Error', function (err) {
-    proxy.logger.error('catchError:', err, err.stack);
-});
+    proxy.logger.error('catchError:', err, err.stack)
+})
 
 /**
  * 浏览根网址时自动跳转到指定地址
@@ -79,11 +84,11 @@ process.on('Error', function (err) {
  *
  */
 proxy.index = function (url) {
-    var index = url || '/index.html';
+    var index = url || '/index.html'
     proxy.when({url: /^[\/\s]*$/}, function (ctx) {
-        ctx.response.redirect(index);
-    });
-    return proxy;
-};
+        ctx.response.redirect(index)
+    })
+    return proxy
+}
 
-module.exports = proxy;
+module.exports = proxy
--- src/middleware/load.js
+++ src/middleware/load.js
@@ -2,72 +2,86 @@
  * 加载网路数据
  * 判断是否是本地文件
  **/
-var request = require('request');
-var fs = require('fs');
+var request = require('request')
+var fs = require('fs')
 
 function handleHeader(header) {
     if (!header) {
-        return;
+        return
     }
     for (var attr in header) {
-        var trim = attr.trim();
+        var trim = attr.trim()
         if (trim.toLowerCase() == 'content-length') {
-            delete header[attr];
-        }
-        else if (trim != attr) {
-            header[trim] = header[attr];
-            delete header[attr];
+            delete header[attr]
+        } else if (trim != attr) {
+            header[trim] = header[attr]
+            delete header[attr]
         }
     }
-    return header;
+    return header
 }
-
 
 module.exports = function (config) {
     return function (ctx, next) {
-        ctx.logger.debug('middleware: load');
+        ctx.logger.debug('middleware: load')
         if (ctx.hasSend() || ctx.isLocal()) {
-            return next();
+            return next()
         }
-        var uri = ctx.fullUrl();
+        var uri = ctx.fullUrl()
 
-        ctx.logger.log('请求网络地址：', uri, '               ');
-        ctx.request.header['accept-encoding'] = 'deflate'; // 取消gzip压缩
+        ctx.logger.log('请求网络地址：', uri, '               ')
+        ctx.request.header['accept-encoding'] = 'deflate' // 取消gzip压缩
         // ctx.request.header['connection'] = 'close'; // 取消keep-alive
         // ctx.request.header['proxy-connection'] = 'close'; // 代理
         // 添加from标签,避免从本地重复请求
         if (ctx.request.header['__from'] == 'koa2-proxy') {
-            return next();
+            return next()
         }
-        ctx.request.header['__from'] = 'koa2-proxy';
+        ctx.request.header['__from'] = 'koa2-proxy'
         var reqdata = {
             uri: uri,
             method: ctx.method,
             headers: handleHeader(ctx.request.header),
             encoding: null
-        };
-        if (ctx.request.body) {
-            reqdata.form = ctx.request.body;
+        }
+        var form = null
+        if (ctx.request.body) form = ctx.request.body
+        if (ctx.request.files) {
+            form = form || {}
+            var files = ctx.request.files
+            for (var attr in files) {
+                var file = files[attr]
+                form[attr] = {
+                    value: fs.createReadScream(file.path),
+                    options: {
+                        filename: file.name,
+                        contentType: file.type
+                    }
+                }
+            }
+        }
+        if (form) {
+            ctx.request.header['content-type'].indexOf('form-data') ? reqdata.formData = form : reqdata.form = form
         }
 
         return new Promise(function (resolve, reject) {
             request(reqdata, function (err, response, body) {
                 try {
                     if (!err) {
-                        ctx.logger.debug('load has response data.');
-                        var header = handleHeader(response.headers);
-                        delete header['content-length']; // 避免长度和设置body长度不一致问题
-                        delete header['transfer-encoding']; // 删除该字段，因为现在是下载完毕处理后才发送
-                        ctx.response.set(header);
-                        ctx.response.body = body;
+                        ctx.logger.debug('load has response data.')
+                        var header = handleHeader(response.headers)
+                        delete header['content-length'] // 避免长度和设置body长度不一致问题
+                        delete header['transfer-encoding'] // 删除该字段，因为现在是下载完毕处理后才发送
+                        ctx.response.set(header)
+                        ctx.response.body = body
                     } else {
-                        ctx.logger.error('middleware load data error: ', err, err.stack);
+                        ctx.logger.error('middleware load data error: ', err, err.stack)
                     }
                 } catch (e) {
-                    ctx.logger.error('koa-proxy middleware load error:', e);
+                    ctx.logger.error('koa-proxy middleware load error:', e)
                 }
-                resolve(next());
-            });
-        });
-    };
+                resolve(next())
+            })
+        })
+    }
 }
